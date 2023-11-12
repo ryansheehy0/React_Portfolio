5f741dcbe74ea60bda8a89486f3b9135c1e83933
Ryan Sheehy <ryansheehy0@gmail.com>
2023-11-12 14:59:16 -0800

    Adds phone and email boxes.

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -6,15 +6,9 @@ export default function Resume(){
     <div className="w-full h-fit flex flex-col items-center">
       <embed src="/ryan_sheehy_resume.pdf" type="application/pdf" className="w-full h-[990px]" />
       <h3 className="text-4xl py-8">Skills</h3>
-      {/*skills.map((skill) => {
-        <Skill {...skill} />
-      })*/}
-        <Skill {...skills[0]} />
-        <Skill {...skills[1]} />
-        <Skill {...skills[2]} />
-        <Skill {...skills[3]} />
-        <Skill {...skills[4]} />
-        <Skill {...skills[5]} />
+      {skills.map((skill) => (
+        <Skill key={skill.skillName} {...skill} />
+      ))}
     </div>
   )
 }
